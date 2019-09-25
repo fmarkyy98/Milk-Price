b29204979ce4519920c3ca1bfd0f06d1a223aa33
Fekete Márk <fmark98@gmail.com>
2019-09-25 16:51:51 +0200

    Done

--- Milk-Price/Milk-Price/Milk-Price.cpp
+++ Milk-Price/Milk-Price/Milk-Price.cpp
@@ -2,7 +2,27 @@
 
 using namespace std;
 
+int A[101];
+
 int main()
 {
-    cout << "Hello World!\n";
+	int N, P, Q;
+	cin >> N >> P >> Q;
+	for (int i = 0; i < N; ++i)
+	{
+		cin >> A[i];
+	}
+
+	int Count = 0;
+
+	for (int i = 0; i < N; i++)
+	{
+		if (P <= A[i] && A[i] <= Q)
+		{
+			++Count;
+		}
+	}
+
+	cout << Count;
+	return 0;
 }
